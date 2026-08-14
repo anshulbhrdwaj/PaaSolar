30ce92118b6f38dfa2f005cb57dd1c234846a404
Umang Bhardwaj <umangbhardwaj0006@gmail.com>
2026-08-14 14:54:27 +0530

    fix: update middleware matcher to exclude static assets and set locale prefix to as-needed in routing configuration

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,8 @@ import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
   locales: ['en', 'hi', 'mr', 'gu', 'ta', 'te', 'kn', 'bn', 'es', 'de'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localePrefix: 'as-needed'
 });
 
 export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
